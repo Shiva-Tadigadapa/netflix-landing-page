a3634501137c83095ddee1b892e37e339558efd9
shiva <shivatadigadapa@gmail.com>
2022-09-08 01:05:29 +0530

    first commit

---
+++ netflix.js
@@ -0,0 +1,21 @@
+
+    const x=document.getElementById("ser").value;
+
+// function myfunction(){
+//     // let x=document.getElementById("ser").value;
+//     let z="Email is required";
+//     let y=" ";
+//     if(x == ""){
+//      ffn2.innerHTML = z;
+//     }
+//     else{
+//         ffn2.innerHTML=x;
+//     }
+//    }
+   function myfunc(){
+    // let email=document.getElementById("ser");
+    document.getElementById("c").innerHTML=="ygyug";
+    ffn2.innerHTML="jfidhui";
+
+   }
+
